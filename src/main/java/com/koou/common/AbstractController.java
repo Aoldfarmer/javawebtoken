f374eb8af2d080668401d1d4eda8f3bf0ab717a2
koou <738648406@qq.com>
2017-05-12 10:02:33 +0800

    新增Controller公用类

---
+++ src/main/java/com/koou/common/AbstractController.java
@@ -0,0 +1,15 @@
+package com.koou.common;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * @author koou
+ * @version 1.0
+ * @since 2017-05-12 上午 9:49
+ */
+public abstract class AbstractController {
+
+    public String currentUserName() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+}
---
+++ src/main/java/com/koou/common/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author koou
+ * @version 1.0
+ * @since 2017-05-12 上午 9:49
+ */
+package com.koou.common;

5e435358ea850b167d044431e2d1822326d08206
yunqiangdi <yunqiangdi@hengtiansoft.com>
2017-06-01 09:54:08 +0800

    update

--- src/test/java/com/koou/ApplicationTests.java
+++ src/test/java/com/koou/ApplicationTests.java
@@ -28,11 +28,33 @@ public class ApplicationTests {
 			XWPFWordExtractor extractor = new XWPFWordExtractor(xdoc);
 			String doc1 = extractor.getText();
 			System.out.println(doc1);
+			String disc = doc1.substring(doc1.indexOf("区域对应板块：") + "区域对应板块：".length(), doc1.indexOf("地铁线路"));
+			String subway = doc1.substring(doc1.indexOf("地铁线路") + "地铁线路".length(), doc1.indexOf("问题："));
+			System.out.println(disc);
+			System.out.println(subway);
+			parse(disc.split("\n"));
+			parse(subway.split("\n"));
+
 			fis.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+
+
+	public void parse(String[] discArray) {
+		for (int i = 0; i < discArray.length; i ++) {
+			if (!discArray[i].equals("")) {
+				System.out.println(discArray[i].substring(0, discArray[i].indexOf("：")));
+				String[] disArrayJ = discArray[i].substring(discArray[i].indexOf("：") + 1).split(" ");
+				for (int j = 0; j < disArrayJ.length; j++) {
+					System.out.println(disArrayJ[j]);
+				}
+			}
+		}
+	}
+
 
 
 }

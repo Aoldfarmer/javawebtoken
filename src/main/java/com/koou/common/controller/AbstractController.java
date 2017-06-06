081c3fba895171e29e1df6769d9741ccba20ce80
yunqiangdi <yunqiangdi@hengtiansoft.com>
2017-06-06 15:49:37 +0800

    update

--- src/main/java/com/koou/common/controller/AbstractController.java
+++ src/main/java/com/koou/common/controller/AbstractController.java
@@ -3,6 +3,7 @@ package com.koou.common.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * @author koou
@@ -15,6 +16,11 @@ public abstract class AbstractController {
     protected Logger log = LoggerFactory.getLogger(getClass());
 
     public String getCurrentUserName() {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
     }
 }

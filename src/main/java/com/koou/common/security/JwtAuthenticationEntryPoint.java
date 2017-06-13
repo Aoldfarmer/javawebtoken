21fbbcb13f44a1115c77eb58e70dc4ba36c1ebe2
yunqiangdi <yunqiangdi@hengtiansoft.com>
2017-06-13 13:14:16 +0800

    update

---
+++ src/main/java/com/koou/common/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.koou.common.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * @author yunqiangdi
+ * @version 1.0
+ * @since 2017-06-13 12:24 PM
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
